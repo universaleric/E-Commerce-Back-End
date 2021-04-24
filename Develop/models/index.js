cf8b1cc808ee7da370e775910731eeac04dd0912
Eric Hay <ericdanielhay@yahoo.com>
2021-04-23 23:51:09 -0500

    Updated index.js within models.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -17,10 +17,22 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag, unique:false });
+Product.belongsToMany(Tag,  {
+  through: {
+    model: ProductTag,
+    unique: false,
+    onDelete: 'CASCADE',
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag, unique:false });
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    onDelete: 'CASCADE'
+  }
+});
 
 module.exports = {
   Product,
